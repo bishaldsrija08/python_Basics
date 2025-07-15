380e8e3b610483bdd99dfd36f904c8376c02d605
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-15 21:30:40 +0545

    first commit

---
+++ day1.py
@@ -0,0 +1,19 @@
+#print statemnent
+# print("Hello World!")
+# cup="Coffee"
+# print(cup)
+
+# =45
+# print()
+
+# b=True
+# print(b)
+
+first_name = "Bishal"
+sir_name = "Rijal"
+print("My first name is "+first_name+" "+sir_name+".")
+
+
+print(f"My first name is {first_name} {sir_name}.")
+
+print("My first name is {} {}.".format(first_name,sir_name))
