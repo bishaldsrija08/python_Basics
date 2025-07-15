b6de723114c42f5a6ac90c91a9c43630041fa6be
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-15 23:36:35 +0545

    list, tuple, set, Dictionaries in python

--- day1.py
+++ day1.py
@@ -17,3 +17,13 @@ print("My first name is "+first_name+" "+sir_name+".")
 print(f"My first name is {first_name} {sir_name}.")
 
 print("My first name is {} {}.".format(first_name,sir_name))
+
+
+# Lis
+friends = ["Bob Marly", "Nick", "Saugat", "Sujan", "Jagrit", True,"Bob Marly", "Nick", "Saugat", "Sujan", "Jagrit", True]
+friends.insert(1,"k xa")
+friends.append("hii")
+friends.pop()
+print (friends[0:2])
+
+print(len(friends))
---
+++ day2.py
@@ -0,0 +1,29 @@
+#Python Dictionaries
+
+friends = {
+    'name': 'Utsab',
+    'age': 22 
+}
+
+# name vanney key xa ki nai check gareko
+if 'name' in friends:
+    print("xa")
+
+del friends['age']
+
+friends['isNepali']=True
+friends['address']="Nepal"
+
+# print(friends['age'])
+print(friends['isNepali'])
+
+print(friends.keys(), friends.values())
+
+
+
+#Python Tuple = immutable, can't change anything
+my_typle = (1,2,3, "bishal", 3.14)
+
+# It ignore duplicacy
+my_set = {1,2,3,21,1}
+print(my_set)
